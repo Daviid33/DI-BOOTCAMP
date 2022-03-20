97f9ac1a5817e9560a00c067813c0435fb922f28
David <dav7@hotmail.fr>
2022-03-20 15:29:34 +0200

    add day1 week2

---
+++ WEEK2/DAY1/Exercice-XP/script.js
@@ -0,0 +1,128 @@
+// Exercise 1
+
+let favfood = "chocolat"
+let favmeal = "dinner"
+let globalmeal = `i eat ${favfood} at every ${favmeal}`
+console.log(globalmeal)
+
+// Exercise 2
+
+let myWatchedSeries = ["black mirror", "money heist", "the big bang theory"];
+let myWatchedSeriesLength = myWatchedSeries.length;
+let globalseries = `i watched 3 series : ${myWatchedSeries}`;
+console.log(globalseries)
+
+ myWatchedSeries.splice(2,1,);
+ console.log(myWatchedSeries)
+
+ myWatchedSeries.push("friends");
+ console.log(myWatchedSeries)
+
+ myWatchedSeries.splice(0,1, "GOT");
+ console.log(myWatchedSeries)
+
+ console.log(myWatchedSeries[1][2]);
+
+
+//  Execercise 3
+
+let c = 17;
+let f = ((c / 5) *9 ) + 32;
+console.log(c);
+console.assert(f);
+console.log(`${c}°c = ${f}°f`);
+
+// Exercise 4
+    let a = 34;
+    let b = 21;
+console.log(a+b)
+
+// 1. 55
+
+// 2. 23
+
+// 3 c=indifine
+
+// 75
+
+// Exercice 5
+
+// 1. number
+// 2. number
+// 3.number
+// 4. string
+// 5. bollean
+// 6.bollean
+// string
+// nan
+// Number
+// Number
+// string
+// nan
+// nan
+// bollean
+// bollean
+// bollean
+
+// Exercise 6
+
+5 + "34"
+// Prediction: 534
+// Actual:
+
+5 - "4"
+// Prediction:1
+// Actual:
+
+10 % 5
+// Prediction: 5
+// Actual:
+
+5 % 10
+// Prediction:5
+// Actual:
+
+"Java" + "Script"
+// Prediction:javascipt
+// Actual:
+
+" " + " "
+// Prediction:nothing
+// Actual:
+
+" " + 0
+// Prediction: `0`
+// Actual:
+
+true + true
+// Prediction: 2
+// Actual:
+
+true + false
+// Prediction:1
+// Actual:
+
+false + true
+// Prediction:1
+// Actual:
+
+false - true
+// Prediction:-1
+// Actual:
+
+!true
+// Prediction:false
+// Actual:
+
+3 - 4
+// Prediction:-1
+// Actual:
+
+"Bob" - "bill"
+// Prediction: nan
+// Actual:
+
+
+
+
+
