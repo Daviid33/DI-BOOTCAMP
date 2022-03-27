a0e87a9e59df3ca2efbe3aa190c8abfbada04102
David <dav7@hotmail.fr>
2022-03-27 16:30:41 +0300

    dailyd1w3

---
+++ WEEK3/DAY1/EXERCISE/script.js
@@ -0,0 +1,12 @@
+
+
+let systems = ["terre", "mars", "pluton", "venus", "jupiter","uranus", "saturne", "neptune"]
+let section = document.querySelector(".listPlanets")
+
+for (let i of systems){
+    let newDiv = document.createElement('div') 
+    let newTextNode = document.createTextNode(i);
+    newDiv.appendChild(newTextNode);
+    newDiv.classList.add("planet")
+    section.appendChild(newDiv)
+}
